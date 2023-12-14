07a74cb82a21e14619667d847587ad11fec6d2c1
akhwang47 <andrewhwang@princeton.edu>
2023-12-13 19:31:43 -0500

    Working on Shortest Job First

--- scheduler/policies/shortest_job_first.py
+++ scheduler/policies/shortest_job_first.py
@@ -1,68 +1,68 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
-import copy
-import random
+import cvxpy as cp
+import numpy as np
 
 import job_id_pair
 from policy import Policy, PolicyWithPacking
 
-# class ShortestJobFirstPolicy(Policy):
-#     def __init__(self, mode='base', seed=None, packing_threshold=1.5):
-#         self._name = 'ShortestJobFirst'
-#         self._mode = mode
-#         self._allocation = {}
-#         self._scale_factors = {}
-#         if mode == 'base':
-#             self._rng = random.Random()
-#             if seed is not None:
-#                 self._rng.seed(seed)
-#         elif mode == 'packing':
-#             self._packing_threshold = packing_threshold
-
-#     def _pack(self, queue, throughputs, scale_factors):
-#         while len(queue) > 0:
-#             # Only make a packing decision if combined normalized
-#             # throughput would provide a signficant gain.
-#             max_packed_throughput = self._packing_threshold
-#             job_id_to_pack_with = None
-#             job_id_to_schedule = queue.pop(0)
-
-#             # Find the already scheduled job with which the next job on
-#             # the queue will pack best with.
-#             for scheduled_job_id in self._allocation:
-#                 assert scheduled_job_id != job_id_to_schedule
-#                 assert scheduled_job_id in throughputs
-#                 if scheduled_job_id.is_pair():
-#                     continue
-#                 if (scale_factors[scheduled_job_id] !=\
-#                         scale_factors[job_id_to_schedule]):
-#                     continue
-#                 worker_type = self._allocation[scheduled_job_id]
-#                 merged_job_id = \
-#                         job_id_pair.JobIdPair(scheduled_job_id[0],
-#                                               job_id_to_schedule[0])
-#                 packed_throughput = throughputs[merged_job_id][worker_type]
-#                 normalized_packed_throughput = 0.0
-#                 for i, single_job_id in enumerate(merged_job_id.singletons()):
-#                     if packed_throughput[i] <= 0.0:
-#                         continue
-#                     isolated_throughput = \
-#                             throughputs[single_job_id][worker_type]
-#                     normalized_packed_throughput += \
-#                             packed_throughput[i] / isolated_throughput
-#                 if normalized_packed_throughput > max_packed_throughput:
-#                     max_packed_throughput = normalized_packed_throughput
-#                     job_id_to_pack_with = scheduled_job_id
-#             if job_id_to_pack_with is None:
-#                 # Terminate when we cannot find a job to pack with.
-#                 # This respects the FIFO property of no jobs being able
-#                 # to jump ahead in the queue.
-#                 return
-#             else:
-#                 # Pack the job with the best job we found.
-
 class ShortestJobFirstPolicy(Policy):
-    def __init__(self, mode='base', seed=None, packing_threshold=1.5):
+    def __init__(self, solver):
         self._name = 'ShortestJobFirst'
-        self._mode = mode
+        self._sjf_perf_policy = \
+            ShortestJobFirstPolicyWithPerf(solver)
+
+    def get_allocation(self, unflattened_throughputs, scale_factors, 
+                       num_steps_remaining, cluster_spec):
+        # Sort jobs by the remaining number of steps
+        sorted_jobs = sorted(num_steps_remaining.items(), key=lambda item: item[1])
+        sorted_job_ids = [job[0] for job in sorted_jobs]
+
+        # Flatten the throughputs as per the cluster spec
+        throughputs, index = self.flatten(unflattened_throughputs, cluster_spec)
+        if throughputs is None:
+            return None
+        (m, n) = throughputs.shape
+        job_ids, worker_types = index
+
+        # Prepare scale factors array
+        scale_factors_array = self.scale_factors_array(scale_factors, job_ids, m, n)
+
+        # Initialize allocation matrix
+        allocation = np.zeros_like(throughputs)
+
+        # Track available capacity in the cluster
+        available_capacity = np.sum(cluster_spec, axis=0)  # Assuming cluster_spec is a 2D array with resource capacities
+
+        for job_id in sorted_job_ids:
+            # Allocate resources for this job
+            job_index = job_ids.index(job_id)
+            job_throughput = throughputs[job_index, :]
+            job_scale_factor = scale_factors_array[job_index, :]
+            
+            # Optimize allocation for this job
+            job_allocation = self.optimize_job_allocation(job_throughput, job_scale_factor, available_capacity)
+
+            # Update the overall allocation and the available capacity
+            allocation[job_index, :] = job_allocation
+            available_capacity -= job_allocation
+
+        return self.unflatten(allocation, index)
+
+    def optimize_job_allocation(self, job_throughput, job_scale_factor, cluster_spec):
+        # Implement the optimization logic here
+        # This method should return the optimal resource allocation for a single job
+
+        # Example optimization code (needs to be adapted to your specific requirements):
+        x = cp.Variable(len(job_throughput))
+        objective = cp.Maximize(job_throughput @ x)
+        constraints = [
+            0 <= x,
+            x <= job_scale_factor,  # Adjust this to match the job's scale factor
+            cp.sum(x) <= np.sum(cluster_spec)  # Total allocation must not exceed total capacity
+        ]
+        prob = cp.Problem(objective, constraints)
+        prob.solve(solver=self._solver)
+
+        return x.value.clip(min=0)  # Ensure non-negative allocation
