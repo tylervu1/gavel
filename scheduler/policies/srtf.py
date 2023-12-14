d09b11575c728ded57f13133901fa1ceeb7c75cf
Tyler Vu <tv3433@princeton.edu>
2023-12-14 04:44:18 -0500

    srtf update

--- scheduler/policies/srtf.py
+++ scheduler/policies/srtf.py
@@ -62,68 +62,53 @@ class SRTFPolicyWithPacking(PolicyWithPacking):
     def __init__(self, seed=None, packing_threshold=1.5):
         super().__init__()
         self._name = 'SRTF_Packing'
+        self._packing_threshold = packing_threshold
+        self._remaining_times = {}
         self._allocation = {}
         self._scale_factors = {}
-        self.remaining_times = {}
-        self._packing_threshold = packing_threshold
         self._rng = random.Random(seed) if seed is not None else random.Random()
 
     def update_remaining_times(self, throughputs):
+        # Update remaining times based on job updates
         for job_id, job_data in throughputs.items():
-            self.remaining_times[job_id] = job_data.remaining_time
+            self._remaining_times[job_id] = job_data['remaining_time']  # Adjust key based on actual data structure
 
-    def _pack(self, queue, throughputs, scale_factors, cluster_spec):
+    def _pack(self, sorted_jobs, throughputs, scale_factors, cluster_spec):
         packed_jobs = set()
-        while queue:
-            job_id_to_schedule = queue.pop(0)
+        for job_id in sorted_jobs:
+            if job_id in packed_jobs or job_id in self._allocation:
+                continue
 
-            if job_id_to_schedule in packed_jobs:
-                continue  # Skip if job is already packed
-
-            max_combined_throughput = 0
+            best_packed_throughput = 0
             best_pair = None
+            for other_job_id in sorted_jobs:
+                if other_job_id == job_id or other_job_id in packed_jobs or other_job_id in self._allocation:
+                    continue
 
-            for other_job_id in queue:
-                if other_job_id in packed_jobs:
-                    continue  # Skip if other job is already packed
+                combined_throughput, worker_type_for_packing = self._calculate_combined_throughput(job_id, other_job_id, throughputs, cluster_spec)
+                if combined_throughput > best_packed_throughput and combined_throughput / (scale_factors[job_id] + scale_factors[other_job_id]) > self._packing_threshold:
+                    best_packed_throughput = combined_throughput
+                    best_pair = (job_id, other_job_id, worker_type_for_packing)
 
-                # Calculate combined throughput if these two jobs are packed
-                for worker_type in cluster_spec:
-                    combined_throughput = (throughputs[job_id_to_schedule][worker_type] +
-                                           throughputs[other_job_id][worker_type])
-                    if combined_throughput > max_combined_throughput:
-                        max_combined_throughput = combined_throughput
-                        best_pair = (job_id_to_schedule, other_job_id)
-
-            if best_pair and max_combined_throughput > self._packing_threshold:
-                # Perform packing
-                job1, job2 = best_pair
+            if best_pair:
+                job1, job2, worker_type = best_pair
                 packed_job_id = job_id_pair.JobIdPair(job1, job2)
-                self._allocation[packed_job_id] = max(cluster_spec, key=cluster_spec.get)  # Assign to the worker type with the most availability
+                self._allocation[packed_job_id] = worker_type
                 packed_jobs.update([job1, job2])
 
     def get_allocation(self, throughputs, scale_factors, cluster_spec):
         available_workers = copy.deepcopy(cluster_spec)
-        queue = sorted(self.remaining_times, key=self.remaining_times.get)
+        sorted_jobs = sorted(self._remaining_times, key=self._remaining_times.get)
 
-        # Allocate resources based on SRTF
-        for job_id in queue:
-            if job_id in self._allocation or job_id in packed_jobs:
-                continue  # Skip already allocated or packed jobs
+        # Allocate resources based on SRTF logic
+        for job_id in sorted_jobs:
+            if job_id in packed_jobs or job_id in self._allocation:
+                continue
 
-            scale_factor = scale_factors[job_id]
-            for worker_type, available in available_workers.items():
-                if available >= scale_factor:
-                    self._allocation[job_id] = worker_type
-                    available_workers[worker_type] -= scale_factor
-                    break
+            self._allocate_job(job_id, scale_factors, available_workers)
 
         # Apply packing logic
-        self._pack(queue, throughputs, scale_factors, available_workers)
+        self._pack(sorted_jobs, throughputs, scale_factors, available_workers)
 
         # Construct final allocation
-        final_allocation = {job_id: {worker_type: 0.0 for worker_type in cluster_spec} for job_id in throughputs}
-        for job_id, worker_type in self._allocation.items():
-            final_allocation[job_id][worker_type] = 1.0
-
-        return final_allocation
+        return self._construct_final_allocation(throughputs, cluster_spec)
